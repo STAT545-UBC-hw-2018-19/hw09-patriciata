c2c6a937031d48aefc3922950f025791194e9e47
ppta <pta@pta.hitronhub.home>
2018-11-27 01:53:46 -0800

    Add comments to R code

--- length.R
+++ length.R
@@ -1,5 +1,9 @@
+# Read in words
 words <- readLines("words.txt")
+# Compute length of each word
 Length <- nchar(words)
+# Find frequencies of each length using the 'table' function
 hist_dat <- table(Length)
+# Save table as a tsv (tab-separated) file
 write.table(hist_dat, "lengths.tsv",
 						sep = "\t", row.names = FALSE, quote = FALSE)
--- starting_letter.R
+++ starting_letter.R
@@ -1,5 +1,9 @@
+# Read in words
 words <- readLines("words.txt")
+# Extract first (starting) letter of each word using substr, and convert to lowercase
 Starting_letter <- tolower(substr(words,1,1))
+# Find frequencies of each starting letter using the 'table' function
 hist_dat <- table(Starting_letter)
+# Save table as a tsv (tab-separated) file
 write.table(hist_dat, "starting_letters.tsv",
 						sep = "\t", row.names = FALSE, quote = FALSE)
